44b17ac625dd38203a5fc5b84cbc53cd69624ddc
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-11-15 02:19:58 -0700

    Simplify LED, add UART demo

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,27 @@
 
 use core::iter::once;
 use embedded_hal::delay::DelayNs;
+use fugit::RateExtU32 as _;
 use panic_halt as _;
 use smart_leds::{brightness, SmartLedsWrite, RGB8};
-use waveshare_rp2040_zero::entry;
 use waveshare_rp2040_zero::{
+    entry,
     hal::{
         clocks::{init_clocks_and_plls, Clock},
+        gpio::FunctionUart,
         pac,
         pio::PIOExt,
         timer::Timer,
+        uart::{DataBits, StopBits, UartConfig, UartPeripheral},
         watchdog::Watchdog,
         Sio,
     },
     Pins, XOSC_CRYSTAL_FREQ,
 };
 use ws2812_pio::Ws2812;
+
+// The trait used by formatting macros like write! and writeln!
+use core::fmt::Write as FmtWrite;
 
 /// Entry point to our bare-metal application.
 ///
@@ -62,7 +68,7 @@ fn main() -> ! {
 
     // Configure the addressable LED
     let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
-    let mut ws = Ws2812::new(
+    let mut onboard_led = Ws2812::new(
         // The onboard NeoPixel is attached to GPIO pin #16 on the Waveshare RP2040-Zero.
         pins.neopixel.into_function(),
         &mut pio,
@@ -71,32 +77,30 @@ fn main() -> ! {
         timer.count_down(),
     );
 
-    // Infinite colour wheel loop
-    let mut n: u8 = 128;
     let mut timer = timer; // rebind to force a copy of the timer
+
+    // Make the onboard LED red.
+    let rgb = RGB8 { r: 255, g: 0, b: 0 };
+    onboard_led.write(brightness(once(rgb), 32)).unwrap();
+
+    let uart_pins = (
+        // UART TX (characters sent from RP2040) on pin 1 (GPIO0)
+        pins.gp0.into_function::<FunctionUart>(),
+        // UART RX (characters received by RP2040) on pin 2 (GPIO1)
+        pins.gp1.into_function::<FunctionUart>(),
+    );
+
+    let mut uart = UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(
+            UartConfig::new(115_200.Hz(), DataBits::Eight, None, StopBits::One),
+            clocks.peripheral_clock.freq(),
+        )
+        .unwrap();
+
     loop {
-        ws.write(brightness(once(wheel(n)), 32)).unwrap();
-        n = n.wrapping_add(1);
+        // Print a message to the UART
+        writeln!(uart, "Hello, world!").unwrap();
 
-        timer.delay_ms(25);
-    }
-}
-
-/// Convert a number from `0..=255` to an RGB color triplet.
-///
-/// The colours are a transition from red, to green, to blue and back to red.
-fn wheel(mut wheel_pos: u8) -> RGB8 {
-    wheel_pos = 255 - wheel_pos;
-    if wheel_pos < 85 {
-        // No green in this sector - red and blue only
-        (255 - (wheel_pos * 3), 0, wheel_pos * 3).into()
-    } else if wheel_pos < 170 {
-        // No red in this sector - green and blue only
-        wheel_pos -= 85;
-        (0, wheel_pos * 3, 255 - (wheel_pos * 3)).into()
-    } else {
-        // No blue in this sector - red and green only
-        wheel_pos -= 170;
-        (wheel_pos * 3, 255 - (wheel_pos * 3), 0).into()
+        timer.delay_ms(1000u32);
     }
 }
